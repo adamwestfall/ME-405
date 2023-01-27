b1ad368fee0445f676cde68b55fb93133e2ef730
DESKTOP-EOU5FQG\jason <jason.davis0311@gmail.com>
2023-01-27 11:02:35 -0800

    More debugging, definitely the input pin tho

--- motor_driver.py
+++ motor_driver.py
@@ -37,19 +37,21 @@ class MotorDriver:
         # initializing timer
         self.timer = timer
         
-    def enable (self):
-        '''   @brief                              Allows the motors to spin
-           @details                            Allows the motors to spin by writing the en_pin
-                                               to logic high
-        '''
-        self.en_pin.high()
+    # def enable (self):
+    #     '''   @brief                              Allows the motors to spin
+    #        @details                            Allows the motors to spin by writing the en_pin
+    #                                            to logic high
+    #     '''
+    #     self.en_pin.high()
     
-    def disable (self):
-        '''   @brief                              Disables motor function
-           @details                            Keeps the motors from spinning by writing the en_pin
-                                               to logic low
-        '''
-        self.en_pin.low()
+    # def disable (self):
+    #     '''   @brief                              Disables motor function
+    #        @details                            Keeps the motors from spinning by writing the en_pin
+    #                                            to logic low
+    #     '''
+    #     self.en_pin.low()
+
+
         
     ## There are several fault conditions that may damage the motor / driver
     ## This function must disable the motor when a fault condition occurs
@@ -71,6 +73,9 @@ class MotorDriver:
         '''
         print('  *** FAULT DETECTED! SUSPENDING L6206 HARDWARE OPERATION ***')
         self.disable()
+        
+    def getEnableState(self):
+        return self.en_pin.value()
     
     # def clearFaultCondition(self):
     #     '''   @brief                           Tests the value of the fault pin 
@@ -201,18 +206,30 @@ class Motor:
         
 if __name__ == '__main__' :
     
-    # defining motor inputs
+    #This is the way to set the condition of the enable pin. Since it is an input, we do not change its value in code.
+    # Instead, we check its value and write code to have the hardware either enable or disable based off of the input 
+    # pin condition.  We need to refactor the existing code to make this possible and delete methods which attempt to
+    # the value of the input.  Lastly, we need to connect an external power supply to the shield and apply ~ 10 volts
+    # to spin the motor.
+    enable1 = pyb.Pin(pyb.Pin.cpu.A10, pyb.Pin.OUT,pull=pyb.Pin.PP)
+    enable2 = pyb.Pin(pyb.Pin.cpu.C1,pyb.Pin.OUT,pull=pyb.Pin.PP)
+
+    #defining motor inputs
+    # refactored for ME 405 hardware
     input1 = pyb.Pin.cpu.B4
     input2 = pyb.Pin.cpu.B5
     input3 = pyb.Pin.cpu.A0
     input4 = pyb.Pin.cpu.A1
-    
-    # creating motor driver / motor objects - wrong
-    m1_driver = motor_driver(pyb.Timer(3, freq = 20000))
-    m1 = motor_driver.motor(input1, input2, 1, 2, "Motor A")
-    m2_driver = motor_driver(pyb.Timer(5, freq = 20000))
-    m2 = motor_driver.motor(input3, input4, 1, 2, "Motor B")
+
+    timer1 = pyb.Timer(3, freq = 20000)
+    timer2 = pyb.Timer(5, freq = 20000)
+
+    #creating motor driver / motor objects
+    # enable pin, input1, input2, timer
+    m1_driver = motor_driver.MotorDriver(enable1, input1, input2, timer1)
+    m1 = m1_driver.motor(input1, input2, 1, 2, "MOTOR A")
+    m2_driver = motor_driver.MotorDriver(enable2, input3, input4, timer2)
+    m2 = m2_driver.motor(input3, input4, 1, 2, "MOTOR B")
     
     # turning on the motor
-    m1_driver.enable()
-    m1_driver.set_duty(75)
+    m1.set_duty(75)
